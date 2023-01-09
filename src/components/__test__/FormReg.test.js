2316ac4c6363b3fc3c9c3fd88dbfa33e88880933
Andrew Aghoghovwia <androyt1@gmail.com>
2023-01-09 16:35:37 +0000

    Added test for input boxes
    Input boxes value should change to whatever the user types in

--- src/components/__test__/FormReg.test.js
+++ src/components/__test__/FormReg.test.js
@@ -1,4 +1,6 @@
 import {screen,render} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import useEvent from '@testing-library/user-event'
 import RegForm from '../RegForm'
 
 describe('Form',()=>{
@@ -29,5 +31,32 @@ describe('Form',()=>{
         expect(confirmPasswordElement).toBeInTheDocument()
         expect(confirmPasswordElement.value).toBe("")
     })
+
+    it('FullName Input Should change value when user types in',()=>{
+        render(<RegForm/>)
+        const fullNameElement=screen.getByPlaceholderText(/fullname/i)
+        userEvent.type(fullNameElement,'Aghoghovwia Andrew')
+        expect(fullNameElement.value).toBe('Aghoghovwia Andrew')
+    })
    
+    it('Email Input Should change value when user types in',()=>{
+        render(<RegForm/>)
+        const emailElement=screen.getByPlaceholderText(/email address/i)
+        userEvent.type(emailElement,'androyt1@gmail.com')
+        expect(emailElement.value).toBe('androyt1@gmail.com')
+    })
+
+    it('Password Input Should change value when user types in',()=>{
+        render(<RegForm/>)
+        const passwordElement=screen.getByPlaceholderText("Password")
+        userEvent.type(passwordElement,'peculiar')
+        expect(passwordElement.value).toBe('peculiar')
+    })
+
+    it('Confirm Password Input Should change value when user types in',()=>{
+        render(<RegForm/>)
+        const cPasswordElement=screen.getByPlaceholderText("Confirm Password")
+        userEvent.type(cPasswordElement,'peculiar')
+        expect(cPasswordElement.value).toBe('peculiar')
+    })
 })
